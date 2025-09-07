97abe9a400353cad2c60a2da73ba3e0075e3309d
Manish Vishwakarma <122515021+HeaLthyDrugs@users.noreply.github.com>
2025-09-07 10:30:14 +0530

    minor changes and added analytics

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,12 @@
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+    <div className="justify-center items-center flex flex-col min-h-screen gap-4">
       <h1>Hello I&apos;m Manish and This is my portfolio</h1>
+      <Link href="/about">About Me</Link>
+      <Link href="/work">Works</Link>
+      <Link href="/blog">Blog</Link>
     </div>
   );
 }
---
+++ app/work/page.tsx
@@ -0,0 +1,8 @@
+export default function WorkPage() {
+  return (
+    <div>
+      <h1>Work Experience</h1>
+      <p>Details about work experience go here.</p>
+    </div>
+  );
+}
