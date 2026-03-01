0ec0f4457f3f8d38b73a8a627cdf14b1bfbffd07
Manish Vishwakarma <122515021+HeaLthyDrugs@users.noreply.github.com>
2026-03-01 22:52:09 +0530

    feat: Implement a command menu with social links, navigation, and theme switching.

--- features/profile/data/social-links.ts
+++ features/profile/data/social-links.ts
@@ -4,37 +4,19 @@ export const SOCIAL_LINKS: SocialLink[] = [
   {
     icon: "https://assets.chanhdai.com/images/link-icons/liquid-glass/linkedin.webp",
     title: "LinkedIn",
-    description: "ncdai",
-    href: "https://linkedin.com/in/ncdai",
+    description: "manishvv",
+    href: "https://www.linkedin.com/in/manishvv/",
   },
   {
     icon: "https://assets.chanhdai.com/images/link-icons/liquid-glass/github.webp",
     title: "GitHub",
-    description: "ncdai",
-    href: "https://github.com/ncdai",
+    description: "HeaLthyDrugs",
+    href: "https://github.com/HeaLthyDrugs",
   },
   {
     icon: "https://assets.chanhdai.com/images/link-icons/liquid-glass/x.webp",
     title: "X (Formerly Twitter)",
-    description: "@iamncdai",
-    href: "https://x.com/iamncdai",
-  },
-  {
-    icon: "https://assets.chanhdai.com/images/link-icons/liquid-glass/dailydotdev.webp",
-    title: "daily.dev",
-    description: "@ncdai",
-    href: "https://app.daily.dev/ncdai",
-  },
-  {
-    icon: "https://assets.chanhdai.com/images/link-icons/liquid-glass/zalo.webp",
-    title: "Zalo",
-    description: "Quaric",
-    href: "https://zalo.me/2353934240045322830",
-  },
-  {
-    icon: "https://assets.chanhdai.com/images/link-icons/liquid-glass/youtube.webp",
-    title: "YouTube",
-    description: "@ncdai",
-    href: "https://www.youtube.com/@ncdai",
+    description: "@HeLLLthyDrugs",
+    href: "https://x.com/HeLLLthyDrug",
   },
 ];
